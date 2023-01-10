89f3d1d88048f9881e8cec00c6d64fee7c6aa31e
javielsitorico <zx21student030@colegiovalledelmiro.es>
2023-01-10 11:25:33 +0100

    JavaScript comenzado, mecanica de juego hecha

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
-for(carta of $('.carta-contenedor')) {
+let parejaActual = []
+let parejasConseguidas = 0;
+let parejas = [
+     ['rayo-1', 'rayo-2'],
+     ['avion-1', 'avion-2'],
+     ['balon-1', 'balon-2'],
+]
+
+for(let carta of $('.carta-contenedor')) {
      $(carta).bind('click', function() {
-          darLaVuelta($(this));
+          revelar($(this));
      });
 }
 
-function darLaVuelta(carta) {
-     if(!carta.hasClass('.carta-contenedor-volteado')) {
-          carta.toggleClass('carta-contenedor-volteado');    
+function revelar(carta) {
+     if(!carta.hasClass('carta-contenedor-volteado')) {
+          carta.toggleClass('carta-contenedor-volteado');
+          parejaActual.push(carta.attr('id'));
+          if(parejaActual.length == 2) comprobarParejas();
      }
 }
+
+function comprobarParejas() {
+     for (let pareja in parejas) {
+          if (parejas[pareja].sort().join(',') == parejaActual.sort().join(',')) {
+               parejas.splice(pareja, 1)
+          }
+     }
+     parejaActual = [];
+     // console.log(parejas);
+     console.log(parejas);
+     if(parejas.length == 0) {
+          console.log('ganaste');
+     }
+}
+
+// elemento.effect( "shake" ); --> Efecto para cuando falle al adivinar
